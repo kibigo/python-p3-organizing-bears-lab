b0f9d0ced74780e10a897c5006cfab12137e0706
kibigo <jacksonkibigo99@gmail.com>
2023-10-25 22:46:51 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,55 @@
+INSERT INTO bears (name,
+age,
+sex,
+color,
+temperament,
+alive) VALUES ('Mr.Chocolate',78,'M','White','Artistic',1);
+
+INSERT INTO bears (name,
+age,
+sex,
+color,
+temperament,
+alive) VALUES ('Rowdy',21,'M','White','Sensitive',0);
+
+INSERT INTO bears (name,
+age,
+sex,
+color,
+temperament,
+alive) VALUES ('Tabitha',32,'F','Black','Moody',1);
+
+INSERT INTO bears (name,
+age,
+sex,
+color,
+temperament,
+alive) VALUES ('Sergeant Brown',41,'M','Black','Persistent',0);
+
+INSERT INTO bears (name,
+age,
+sex,
+color,
+temperament,
+alive) VALUES ('Melissa',65,'F','White','Moody',1);
+
+INSERT INTO bears (name,
+age,
+sex,
+color,
+temperament,
+alive) VALUES ('Grinch',50,'F','White','Artistic',0);
+
+INSERT INTO bears (name,
+age,
+sex,
+color,
+temperament,
+alive) VALUES ('Wendy',50,'F','Black','Moody',1);
+
+INSERT INTO bears (name,
+age,
+sex,
+color,
+temperament,
+alive) VALUES (NULL,NULL,'F',NULL,NULL,NULL);
